e368d791b58b6fd589558697c60c588548be8978
Cahit Arslan <cahitarslan.business@gmail.com>
2022-07-04 16:35:35 +0300

    initial

---
+++ translate_api.js
@@ -0,0 +1,26 @@
+class TranslateApi {
+    constructor(ingilizceSaka) {
+        this.baseURL = 'https://translation.googleapis.com';
+
+        this.aranacakCumle = ingilizceSaka;
+        this.axiosNesnesi = axios.create({
+            baseURL: this.baseURL,
+            params: {
+                target: 'tr',
+                key: 'AIzaSyCGG-hgYs08Ts1bKSsa5IOiwBRJYfhXAWxQ',
+                q: this.aranacakCumle,
+            },
+        });
+    }
+
+    async ceviriYap() {
+        try {
+            const ceviri = await this.axiosNesnesi.get('/language/translate/v2');
+            console.log(ceviri.data.data.translations[0].translatedText);
+            return ceviri.data.data.translations[0].translatedText;
+        } catch (hata) {
+            console.log(hata.response.data.error.message);
+            return hata.response.data.error.message;
+        }
+    }
+}
---
+++ unsplash_api.js
@@ -0,0 +1,27 @@
+class UnsplashApi {
+    constructor() {
+        this.baseURL = 'https://api.unsplash.com';
+        this.clientID = 'Client-ID qZnKTivBVH9p1WsMogtEMGIc8KrlNKViGgSUaNHSKxA';
+        this.axiosNesne = axios.create({
+            baseURL: this.baseURL,
+            headers: {
+                Authorization: this.clientID,
+            },
+            params: {
+                query: 'joker joke',
+                count: 1,
+            },
+        });
+    }
+
+    async randomResimGetir() {
+        try {
+            const resimResponse = await this.axiosNesne.get('/photos/random');
+            console.log(resimResponse.data[0].urls.regular);
+            return resimResponse.data[0].urls.regular;
+        } catch (hata) {
+            console.log(hata.response);
+            return 'https://miro.medium.com/max/1838/1*MIrLuyiCDpdNbnFYxYlKtA.png';
+        }
+    }
+}
